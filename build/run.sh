5e49af9bed8401e634a595a76113290a8bf4acfc
Justin Merz <jrmerz@gmail.com>
2022-07-22 14:24:48 -0700

    tmp removal of git env vars

--- build/run.sh
+++ build/run.sh
@@ -10,9 +10,10 @@ cd $ROOT_DIR
 source config.sh
 cd ..
 
-A6T_REPO_HASH=$(git log -1 --pretty=%h)
-A6T_REPO_TAG=$(git describe --tags --abbrev=0 || '')
-A6T_REPO_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+# TODO.  need custom build container
+# A6T_REPO_HASH=$(git log -1 --pretty=%h)
+# A6T_REPO_TAG=$(git describe --tags --abbrev=0 || '')
+# A6T_REPO_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 ##
 # Harvest
@@ -24,10 +25,11 @@ docker build \
   -t $A6T_IMAGE_NAME:$CONTAINER_CACHE_TAG \
   --build-arg NODE_VERSION=${NODE_VERSION} \
   --build-arg KAFKA_IMAGE=${KAFKA_IMAGE_NAME_TAG} \
-  --build-arg A6T_REPO_HASH=${A6T_REPO_HASH} \
-  --build-arg A6T_REPO_TAG=${A6T_REPO_TAG} \
-  --build-arg A6T_REPO_BRANCH=${A6T_REPO_BRANCH} \
-  --cache-from=$A6T_IMAGE_NAME:$CONTAINER_CACHE_TAG \
+    --cache-from=$A6T_IMAGE_NAME:$CONTAINER_CACHE_TAG \
   .
+  # --build-arg A6T_REPO_HASH=${A6T_REPO_HASH} \
+  # --build-arg A6T_REPO_TAG=${A6T_REPO_TAG} \
+  # --build-arg A6T_REPO_BRANCH=${A6T_REPO_BRANCH} \
+
 
 docker tag $A6T_IMAGE_NAME_TAG $A6T_IMAGE_NAME:$A6T_REPO_BRANCH
