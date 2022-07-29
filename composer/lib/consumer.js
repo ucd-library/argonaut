4c86003f898c1b5c230b02c4e682b83b021bd8a6
Justin Merz <jrmerz@gmail.com>
2022-07-29 16:00:38 -0700

    adding autoCommitInterval

--- composer/lib/consumer.js
+++ composer/lib/consumer.js
@@ -54,6 +54,7 @@ class A6tConsumer extends EventEmitter {
   async _listen() {
     try {
       await this.kafkaConsumer.run({
+        autoCommitInterval: 5000,
         eachMessage: async ({topic, partition, message, heartbeat, pause}) => {
           try {
             await this._onMessage(message, topic, partition, message)
